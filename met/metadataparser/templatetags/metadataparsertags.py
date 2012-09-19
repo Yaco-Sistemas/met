7e942efd21feea481f8501c04d3dce87b40bf552
ant30 <ant30tx@gmail.com>
2012-09-19 11:56:31 +0200

    Implemented bootstrap forms and simple headers block

--- met/metadataparser/templatetags/metadataparsertags.py
+++ met/metadataparser/templatetags/metadataparsertags.py
@@ -5,5 +5,7 @@ register = template.Library()
 
 
 @register.inclusion_tag('metadataparser/bootstrap_form.html')
-def bootstrap_form(form):
-    return {'form': form}
+def bootstrap_form(form, cancel_link, delete_link=None):
+    return {'form': form,
+            'cancel_link': cancel_link,
+            'delete_link': delete_link}
